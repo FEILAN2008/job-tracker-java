ff6c1788eb372c4ec9b145313612fe74aa873e8f
FEILAN2008 <nihao.lanfei@gmail.com>
2025-04-21 00:26:04 +1200

    chore: save local changes before merge

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mdd3c14la8o"
+    "revision": "0.53sp3a8kd28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
